f5595484ba046fe8410f7f2d091215453707ac16
Mohit Munjani <mohitcrox@gmail.com>
2018-08-08 11:34:49 +0530

    jenkins

--- conf/single.conf.js
+++ conf/single.conf.js
@@ -9,5 +9,8 @@ exports.config = {
     'browserName': 'chrome',
     'resolution': '1024x768',
     'browserstack.debug': 'true'
-  }
+  },
+  'plugins': [{
+    'package': "browserstack-protractor-reporter"
+  }]
 };
